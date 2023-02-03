b3b4c4add4aad6d196458b8e9ad3b9b39db620eb
William Russell <williamrussell@Williams-MacBook-Pro.local>
2023-02-03 18:22:20 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: 'Bill',
+    streetAddress: '318 West 125th Street'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employees = {...employee};
+    employees[`${key}`] = `${value}`;
+    return employees;
+  }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee [`${key}`] = `${value}`;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employees = {...employee};
+    delete employees[`${key}`];
+    return employees;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[`${key}`];
+    return employee;
+}
